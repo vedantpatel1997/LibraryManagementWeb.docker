7ad63895253f0f6aa6f933160f49f9a054866bee
vedantpatel1997 <vedantp9@gmail.com>
2024-05-21 13:18:47 -0600

    init

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   secret: 'Vedant@1997',
-  apiAddress: 'https://libraryconestoga.azurewebsites.net/api/',
+  apiAddress: 'https://libraryapivp.azurewebsites.net/api/',
+  // apiAddress: 'https://libraryconestoga.azurewebsites.net/api/',
 };
