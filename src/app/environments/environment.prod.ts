f783cee5fd45134b2eec792c231f91eddaa88fcc
vedantpatel1997 <vedantp9@gmail.com>
2024-06-26 17:48:05 -0600

    remove env folder

--- src/app/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-// export const environment = {
-//   production: true,
-//   secret: 'Vedant@1997',
-//   apiAddress: 'https://libraryapivp.azurewebsites.net/api/',
-//   // apiAddress: 'https://libraryconestoga.azurewebsites.net/api/',
-// };
--- src/app/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-// export const environment = {
-//   production: false,
-//   apiAddress: 'https://localhost:7026/api/',
-//   secret: 'Vedant@1997',
-// };
